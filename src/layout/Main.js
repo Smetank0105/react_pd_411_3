6924d9729e5d3756d7228bd36439cda72d55bb52
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2026-02-18 19:35:08 +0700

    Serach & Types

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -2,6 +2,7 @@ import './Main.css';
 import React from 'react';
 import MovieList from '../components/MovieList.js';
 import Preloader from '../components/Preloader.js';
+import Search from '../components/Search.js';
 
 class Main extends React.Component
 {
@@ -17,12 +18,22 @@ class Main extends React.Component
         .then(data => this.setState({movies: data.Search}));
     }
 
+    searchMovie = (str, type='all') =>
+    {
+        fetch(`https://omdbapi.com/?apikey=1430ab10&s=${str}${type != 'all' ? `&type=${type}` : ''}`)
+        .then(response => response.json())
+        .then(data => this.setState({movies: data.Search}));
+    }
+
     render()
     {
         return(
             <div className='main'>
                 <div className='wrap'>
-                    {this.state.movies.length === 0 ? <Preloader /> : <MovieList movies={this.state.movies} />}
+                    <Search searchMovie={this.searchMovie} />
+                    {
+                        this.state.movie != null && this.state.movies.length === 0 ? <Preloader /> : <MovieList movies={this.state.movies} />
+                    }
                 </div>
             </div>
         )
